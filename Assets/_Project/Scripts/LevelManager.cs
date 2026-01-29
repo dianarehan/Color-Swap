621c62a24b1a5fb695aa14d4233eda24d069e3a9
diana <daiana.rehan.dr@gmail.com>
2026-01-29 16:09:25 +0200

    Add Win panel

--- Assets/_Project/Scripts/LevelManager.cs
+++ Assets/_Project/Scripts/LevelManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections.Generic;
 
 public class LevelManager : MonoBehaviour
@@ -6,22 +7,77 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private int currentLevelIndex = 0;
     [SerializeField] private LevelData[] levelDataArray; 
     [SerializeField] private GameObject[] Levels;
-    bool isLevelComplete = false;
-    bool allLevelsCompleted = false;
+    
+    [SerializeField] private GameObject winPanel;
+    [SerializeField] private Button nextButton;
+    [SerializeField] private Button previousButton;
+
+    private bool isLevelComplete = false;
+    private bool allLevelsCompleted = false;
     private Node[] allNodesList; 
 
     void Start()
     {
-        GetAllNodes();
+        if (winPanel != null) winPanel.SetActive(false);
+        
+        InitializeLevel(currentLevelIndex);
+        UpdateUIState();
     }
     
-    void GetAllNodes()
+    void InitializeLevel(int index)
     {
-        allNodesList = Levels[currentLevelIndex].GetComponentsInChildren<Node>();
+        for (int i = 0; i < Levels.Length; i++)
+        {
+            if (Levels[i] != null) Levels[i].SetActive(i == index);
+        }
+        
+        if (Levels[index] != null)
+        {
+            Transform nodesContainer = Levels[index].transform.Find("Nodes");
+            if (nodesContainer != null)
+                allNodesList = nodesContainer.GetComponentsInChildren<Node>();
+            else
+                allNodesList = Levels[index].GetComponentsInChildren<Node>();
+        }
+        
+        isLevelComplete = false;
+        if (winPanel != null) winPanel.SetActive(false);
+    }
+    
+    public void LoadNextLevel()
+    {
+        if (currentLevelIndex < Levels.Length - 1)
+        {
+            currentLevelIndex++;
+            InitializeLevel(currentLevelIndex);
+            UpdateUIState();
+        }
+        else
+        {
+            Debug.Log("Already at max level");
+        }
+    }
+
+    public void LoadPreviousLevel()
+    {
+        if (currentLevelIndex > 0)
+        {
+            currentLevelIndex--;
+            InitializeLevel(currentLevelIndex);
+            UpdateUIState();
+        }
+    }
+
+    void UpdateUIState()
+    {
+        if (previousButton != null) previousButton.interactable = (currentLevelIndex > 0);
+        if (nextButton != null) nextButton.interactable = (currentLevelIndex < Levels.Length - 1);
     }
 
     public bool IsConnected(Node a, Node b)
     {
+        if (currentLevelIndex >= levelDataArray.Length) return false;
+
         foreach (var conn in levelDataArray[currentLevelIndex].connections)
         {
             if ((conn.nodeAIndex == a.nodeIndex && conn.nodeBIndex == b.nodeIndex) ||
@@ -62,23 +118,12 @@ public class LevelManager : MonoBehaviour
     {
         isLevelComplete = true;
         Debug.Log("Excellence!");
-        LoadNextLevel();
-    }
-
-    void LoadNextLevel()
-    {
-        if (allLevelsCompleted) return;
-        Levels[currentLevelIndex].SetActive(false);
-        currentLevelIndex++;
-        if (currentLevelIndex >= Levels.Length)
+        
+        // Show Win Panel instead of auto-loading
+        if (winPanel != null)
         {
-            allLevelsCompleted = true;
-            Debug.Log("All levels completed!");
-            return;
+            winPanel.SetActive(true);
+            UpdateUIState(); // Refresh buttons based on current level
         }
-
-        Levels[currentLevelIndex].SetActive(true);
-        GetAllNodes();
-        isLevelComplete = false;
     }
 }
